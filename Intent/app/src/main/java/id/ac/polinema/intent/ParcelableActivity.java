0da2a8a6b97902b43ebbea9726eb390b1df844ae
soyidwahyud <soyidwahyu4@gmail.com>
2019-09-18 17:00:45 +0700

    Parcelable Activity dan buat package model dengan class User

--- Intent/app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
@@ -2,14 +2,38 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import id.ac.polinema.intent.model.User;
 
 public class ParcelableActivity extends AppCompatActivity {
 
+    private EditText usernameInput;
+    private EditText nameInput;
+    private EditText ageInput;
+
+    User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parcelable);
+        usernameInput = findViewById(R.id.input_username);
+        nameInput = findViewById(R.id.input_name);
+        ageInput = findViewById(R.id.input_age);
     }
 
+    public void handleProfilParcelable(View view) {
+        String username = usernameInput.getText().toString();
+        String name = nameInput.getText().toString();
+        int age = Integer.parseInt(ageInput.getText().toString());
+        user = new User(username, name, age);
+
+        Intent intent = new Intent(this, ProfileParcelableActivity.class);
+        intent.putExtra("soyid", user);
+
+        startActivity(intent);
+    }
 }
--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
@@ -3,20 +3,37 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import id.ac.polinema.intent.model.User;
 
 public class ProfileParcelableActivity extends AppCompatActivity {
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_parcelable);
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
         // TODO: bind here
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-
             // TODO: display value here
+            User user = getIntent().getParcelableExtra("soyid");
+
+            String username = user.getUsername();
+            String name = user.getName();
+            int age = user.getAge();
+
+            usernameText.setText(username);
+            nameText.setText(name);
+            ageText.setText(String.valueOf(age));
         }
     }
 }
