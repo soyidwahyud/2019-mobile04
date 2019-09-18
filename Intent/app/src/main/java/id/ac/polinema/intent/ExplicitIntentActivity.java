a06b6c642ca3ac999d5c7b705116f18732ee8540
soyidwahyud <soyidwahyu4@gmail.com>
2019-09-18 14:23:16 +0700

    menambahkan on click di activity_main dan activity_explicit dan menambahkan code di mainActivity dan ExplicitIntentActivity

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -8,11 +8,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
-
+    private EditText editText;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        editText = findViewById(R.id.input_name);
+        textView = findViewById(R.id.text_output);
     }
 
+
+    public void submitOnClick(View view) {
+        String name = editText.getText().toString();
+        textView.setText("Hello " + name + ", Congratulations!");
+    }
 }
