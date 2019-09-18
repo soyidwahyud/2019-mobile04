2ee1f6d77f643b2a3814cdfb8fcb81075c060074
soyidwahyud <soyidwahyu4@gmail.com>
2019-09-18 15:04:25 +0700

    menambahkan on click di activity_main, activity Explicit intent, Activity implisit Intent xml dan class

--- Intent/app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -19,9 +19,14 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void handleImplicitIntent(View view) {
+        Intent intent = new Intent(this, ImplicitIntentActivity.class);
+        startActivity(intent);
+    }
     public void handleExit(View view) {
         finish();
     }
+
 
 
 }
